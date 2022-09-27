c4b14f1724735fbd4803c418354741fbea54338f
Karthik <karthik5112001@gmail.com>
2022-09-27 08:39:41 +0530

    commit

--- src/components/Data/S_Booking_result.js
+++ src/components/Data/S_Booking_result.js
@@ -10,6 +10,18 @@ const data=[
         top_off:" Get FLAT 25% off on SEATS!",
         bottom_off:"Upto 35% off with Axis/ICICI cards (BBDCC)",
 
+    },
+    {
+        flight:"quien",
+        serNo:"SG-123",
+        departure:"23:55",
+        duration:"9h 40m",
+        arrival:"09:35",
+        price:"₹9,875",
+        top_off_bold:"CT Upgrade",
+        top_off:" Get FLAT 25% off on SEATS!",
+        bottom_off:"Upto 35% off with Axis/ICICI cards (BBDCC)",
+
     }
 ]
 export default data;
--- src/components/pages/S_Booking.js
+++ src/components/pages/S_Booking.js
@@ -2,7 +2,7 @@ import React from 'react'
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Select from '@mui/material/Select';
 
 import { RiArrowLeftRightFill } from 'react-icons/ri'
 
@@ -19,6 +19,22 @@ import data from '../Data/S_Booking_result'
 import '../css/S_Booking.css'
 
 
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+
+
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiDialogContent-root': {
+        padding: theme.spacing(2),
+    },
+    '& .MuiDialogActions-root': {
+        padding: theme.spacing(1),
+    },
+}));
+
 function S_Booking() {
     const [value, setValue] = React.useState(dayjs());
     console.log("booking result " + value);
@@ -26,6 +42,23 @@ function S_Booking() {
         setValue(newValue);
         console.log(value)
     }
+
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+
+
+
+
+
+
 
     return (
         <div>
@@ -117,39 +150,102 @@ function S_Booking() {
             {/* search booking result */}
 
             <div className="result_main_cont">
-
-
-                {/* flight booking detail */}
-                {data.map((item, index) => {
-                    <div className="result_cont">
-                        <p className='r_d1p1'><span>{data[index].top_off_bold}</span> Get FLAT 25% off on SEATS!</p>
-                        <div className='r_d1'>
-                            <div className='r_d2'>
-                                <img src='https://firebasestorage.googleapis.com/v0/b/flight-booking-d4fcf.appspot.com/o/SG_2x.png?alt=media&token=754f59b8-35f7-4f69-ab78-cf1885b68d30' />
-                                <div className='r_d3'>
-                                    <p>SpiceJet</p>
-                                    <p>SG-123</p>
+                {/* travel details */}
+                {data.map((item) => {
+                    return (
+                        <div className="result_cont">
+                            <p className='r_d1p1'><span>{item.top_off_bold}</span> Get FLAT 25% off on SEATS!</p>
+                            <div className='r_d1'>
+                                <div className='r_d2'>
+                                    <img src='https://firebasestorage.googleapis.com/v0/b/flight-booking-d4fcf.appspot.com/o/SG_2x.png?alt=media&token=754f59b8-35f7-4f69-ab78-cf1885b68d30' />
+                                    <div className='r_d3'>
+                                        <p>{item.flight}</p>
+                                        <p>SG-123</p>
+                                    </div>
                                 </div>
                             </div>
+                            <p className='r_t1'>23:55</p>
+                            <p>9h 40m</p>
+                            <p>09:35</p>
+                            <h3>{item.price}</h3>
+                            <div className='f_b2'>
+                                <Stack spacing={2} direction="row" >
+                                    <Button variant="contained" onClick={handleClickOpen}>Book here</Button>
+                                </Stack>
+                                <p className='r_d1_p'>Flight Details</p>
+                            </div>
+                            <p className='off1'>Upto 35% off with Axis/ICICI cards (BBDCC)</p>
                         </div>
-                        <p className='r_t1'>23:55</p>
-                        <p>9h 40m</p>
-                        <p>09:35</p>
-                        <h3>₹11,875</h3>
-                        <div className='f_b2'>
-                            <Stack spacing={2} direction="row" >
-                                <Button variant="contained">Search</Button>
-                            </Stack>
-                            <p className='r_d1_p'>Flight Details</p>
-                        </div>
-                        <p className='off1'>Upto 35% off with Axis/ICICI cards (BBDCC)</p>
-                    </div>
+                    )
                 })
 
                 }
-
-
             </div>
+
+
+
+            {/* pop up window */}
+
+            <div>
+                <BootstrapDialog
+                    onClose={handleClose}
+                    aria-labelledby="customized-dialog-title"
+                    open={open}
+                >
+                    <form>
+
+                        <DialogContent>
+                            <DialogContentText>
+                                Enter your details
+                            </DialogContentText>
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                id="name"
+                                label="Name"
+                                type="text"
+                                fullWidth
+                                variant="standard"
+                            />
+                        </DialogContent>
+                        <DialogContent>
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                id="name"
+                                label="Email Address"
+                                type="email"
+                                fullWidth
+                                variant="standard"
+                            />
+                        </DialogContent>
+                        <DialogContent>
+                            <TextField
+                                autoFocus
+                                margin="dense"
+                                id="name"
+                                label="phone nu"
+                                type="number"
+                                fullWidth
+                                variant="standard"
+                                required
+                            />
+
+                        </DialogContent>
+                        <DialogContent>
+
+                            <Stack spacing={2} direction="row" className='f_b'>
+                                <Button variant="contained">Book now</Button>
+                            </Stack>
+                        </DialogContent>
+                    </form>
+
+                </BootstrapDialog>
+            </div>
+
+
+
+
         </div>
     )
 }
