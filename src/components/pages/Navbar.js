7d58faeb2ea9ca56ee9f890308f915d74800b240
Karthik <karthik5112001@gmail.com>
2022-10-02 19:33:17 +0530

    commit

--- src/components/pages/Navbar.js
+++ src/components/pages/Navbar.js
@@ -1,49 +1,53 @@
 import React from 'react'
 import '../css/Navbar.css'
 import { MdOutlineFlight } from 'react-icons/md'
-import { RiHotelLine,RiPassportLine } from 'react-icons/ri'
-import {TbDiscount2} from 'react-icons/tb'
-import {FiPhoneCall} from 'react-icons/fi'
-import  {IoIosContact} from 'react-icons/io'
- 
+import { RiHotelLine, RiPassportLine } from 'react-icons/ri'
+import { TbDiscount2 } from 'react-icons/tb'
+import { FiPhoneCall } from 'react-icons/fi'
+import { IoIosContact } from 'react-icons/io'
+import {MdFlight} from 'react-icons/md'
+
 
 function Navbar() {
     return (
         <div>
-            <div className="nav_cont">
-                <h1>SkyTrip</h1>
+            <div className='nav_main_cont'>
+                <div className="nav_cont logo">
+                    <h1><MdFlight className='logo_fli' />SkyTrip</h1>
 
-                <div className='i_cont'>
-                    <div className='f_i_cont'>
-                        <MdOutlineFlight className='n_icons' />
-                        <p>Flights</p>
+                    <div className='i_cont'>
+                        <div className='f_i_cont'>
+                            <MdOutlineFlight className='n_icons' />
+                            <p>Flights</p>
+                        </div>
+                        <div className='f_i_cont'>
+                            <RiHotelLine className='n_icons' />
+                            <p>Hotels</p>
+                        </div>
+                        <div className='f_i_cont'>
+                            <RiPassportLine className='n_icons' />
+                            <p>Passport</p>
+                        </div>
                     </div>
-                    <div className='f_i_cont'>
-                        <RiHotelLine className='n_icons'/>
-                        <p>Hotels</p>
+
+                    <div className='i_cont'>
+                        <div className='f_i_cont'>
+                            <TbDiscount2 className='n_icons' />
+                            <p>Offers</p>
+                        </div>
+                        <div className='f_i_cont'>
+                            <FiPhoneCall className='n_icons' />
+                            <p id="call_icon">Customer services</p>
+                        </div>
+                        <div className='f_i_cont login'>
+                            <IoIosContact className='n_icons' />
+                            <p>Login/SignUp</p>
+                        </div>
                     </div>
-                    <div className='f_i_cont'>
-                        <RiPassportLine className='n_icons'/>
-                        <p>Passport</p>
-                    </div>
+
                 </div>
-
-                <div className='i_cont'>
-                    <div  className='f_i_cont'>
-                        <TbDiscount2 className='n_icons'/>
-                        <p>Offers</p>
-                    </div>
-                    <div  className='f_i_cont'>
-                        <FiPhoneCall className='n_icons'/>
-                        <p id="call_icon">Customer services</p>
-                    </div>
-                    <div  className='f_i_cont'>
-                        <IoIosContact className='n_icons'/>
-                        <p>Login/SignUp</p>
-                    </div>
-                </div>
-
             </div>
+
         </div>
     )
 }
